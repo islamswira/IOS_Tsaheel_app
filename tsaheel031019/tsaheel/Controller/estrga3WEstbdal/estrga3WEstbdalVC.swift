cfd4d5731b260ecb37afab6440c36878dbd0c9a3
Islam Swira <islamswira93@gmail.com>
2019-10-08 20:11:44 +0200

    ٨-١٠-٢٠١٩

---
+++ tsaheel031019/tsaheel/Controller/estrga3WEstbdal/estrga3WEstbdalVC.swift
@@ -0,0 +1,30 @@
+//
+//  estrga3WEstbdalVC.swift
+//  tsaheel
+//
+//  Created by Islam Swira on 10/8/19.
+//  Copyright © 2019 com.Islam Swira. All rights reserved.
+//
+
+import UIKit
+
+class estrga3WEstbdalVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
